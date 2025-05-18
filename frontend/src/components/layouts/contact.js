7c932d0d10573c611554c89ec2215447e970973b
22adr007 <lokitha02@gmail.com>
2025-05-18 22:03:12 +0530

    Textile_shop

---
+++ frontend/src/components/layouts/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container mt-5">
+            <h2>Contact Information</h2>
+            <p><strong>STORE NAME:</strong> SRI SAI COLLECTION</p>
+            <p><strong>ADDRESS:</strong> SRI SAI COMPLEX, KUPPANDAMPALAYAM, VEERAPANDI POST, TIRUPUR-641 605</p>
+            <p><strong>PHONE NUMBER:</strong> +91 99442 96162</p>
+            <p><strong>MAIL ID:</strong> ganeshkumar@gmail.com</p>
+        </div>
+    );
+}
+
+export default Contact;
